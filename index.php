a3f9bf55afcbb7033d77ee9ec40d6d75f187f8a4
Troels Reese Christensen <troelsreesechristensen@Frederikkes-iMac.local>
2016-09-09 07:52:01 +0200

    index file added

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php //require('includes/config.php'); ?>
+<?php $pageName="Home"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Trolzie</title>
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<?php //include('components/navigation.php');?>
+		</div>
+	</header>
+
+	<section>
+
+		<h1>Hello World</h1>
+
+	</section>
+
+</body>
+</html>
