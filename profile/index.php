8343e2dce491c83a5f7221d82983d08e74300c8d
Troels Reese Christensen <tlrc1984@gmail.com>
2016-09-12 07:57:27 +0200

    profile wip, update user info

--- profile/index.php
+++ profile/index.php
@@ -1,6 +1,11 @@
 <?php require_once('../config.php');
-echo "user id: " . $_SESSION['userID'] . ", ";
-echo "logged in: " . $_SESSION['loggedin'];
+
+if ($_SESSION['loggedin'] == 1) {
+	echo "logged in, " . "user id: " . $_SESSION['userID'];
+} else {
+	echo "logged out";
+}
+
 //if not logged in redirect to login page
 
 // if(!$user->is_logged_in()){ header('Location: ../index.php'); }
@@ -25,10 +30,72 @@ $row = $stmt->fetch();
 
 	<h2>Your current profile:</h2>
 
+	<?php
+
+		if(isset($_POST['submit'])){
+
+			$_POST = array_map( 'stripslashes', $_POST );
+
+			//collect form data
+			extract($_POST);
+
+			// if($postID ==''){
+			// 	$error[] = 'This post is missing a valid id!.';
+			// }
+
+			// if($postTitle ==''){
+			// 	$error[] = 'Please enter the title.';
+			// }
+
+			// if($postDesc ==''){
+			// 	$error[] = 'Please enter the description.';
+			// }
+
+			// if($postCont ==''){
+			// 	$error[] = 'Please enter the content.';
+			// }
+
+			echo $userHeading;
+			echo $userSubheading;
+			echo $userBody;
+			echo "-   ";
+			echo $userKey;
+			echo $row['userKey'];
+			echo "   -";
+
+			if(!isset($error)){
+
+				try {
+
+					//insert into database
+					$stmt = $db->prepare('UPDATE owc_userdata SET userHeading = :userHeading, userSubheading = :userSubheading, userBody = :userBody, WHERE userKey = :userKey') ;
+					$stmt->execute(array(
+						':userHeading' => $userHeading,
+						':userSubheading' => $userSubheading,
+						':userBody' => $userBody,
+						':userKey' => $userKey
+					));
+
+					// redirect to index page
+					// header('Location: index.php?action=updated');
+					header('Location: index.php');
+					exit;
+
+				} catch(PDOException $e) {
+				    echo $e->getMessage();
+				}
+
+			}
+
+		}
+
+	?>
+
 	<form action="" method="post">
-		<label>Name</label><input type="text" name="name" value="<?php echo $row['userHeading']; ?>"/><br>
-		<label>Heading</label><input type="text" name="heading" value="<?php echo $row['userSubheading']; ?>"/><br>
-		<label>Body</label><textarea type="text" name="body" rows="6" cols="50"/><?php echo $row['userBody']; ?></textarea><br>
+		<input type='hidden' name='userKey' value='<?php echo $row['userKey'];?>'>
+		<label>userHeading</label><input type="text" name="userHeading" value="<?php echo $row['userHeading']; ?>"/><br>
+		<label>userSubheading</label><input type="text" name="userSubheading" value="<?php echo $row['userSubheading']; ?>"/><br>
+		<label>userBody</label><textarea type="text" name="userBody" rows="6" cols="50"/><?php echo $row['userBody']; ?></textarea><br>
 		<label></label><input type="submit" name="submit" value="Save"/><br>
 	</form>
 
